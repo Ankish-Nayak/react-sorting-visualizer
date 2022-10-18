755fee3991964845ff7c78b81ad556d59f29cf59
Ankish-Nayak <ankish2000nayak@gmail.com>
2022-10-18 10:18:40 +0530

    First somewhat stable

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Sidebar from "./Sidebar";
 import Main from "./Main";
 
@@ -14,15 +14,61 @@ export default function App() {
     } 
     return res;
   }  
+  // useEffect(()=>{
+  //   return ()=>{
+  //     clearTimeout()
+  //   }
+  // },[])
   function sortBars() { 
-    setBars((prevBars) => {
-      let res = [];
-      for (let i = 0; i < prevBars.length; ++i) {
-        res.push(prevBars[i]);
-      } 
-      res.sort((a, b) => a - b); 
-      return res;
-    });
+    let steps = []; // we will use this store all steps in particular algorithm took to sort the array
+    // 
+    
+    let array = [...bars];
+    let size = array.length;
+    steps.push(array);
+    const sleep = ms => new Promise(resolve => setTimeout(resolve,ms));
+    (async () =>{
+      for(let step = 1; step<size; ++step){
+        let key = array[step];
+        let j = step-1;
+ 
+        await sleep(20);
+        while(key < array[j] && j >= 0){ 
+          setBars([...array]);
+          await sleep(20); 
+
+          array[j+1] = array[j];
+          setBars([...array]);
+          await sleep(20);
+          j--;
+        } 
+        array[j + 1] = key; 
+        setBars([...array]);
+        await sleep(20);
+      }
+    })();
+    // for (let step = 1; step < size; step++) {
+    //   let key = array[step];
+    //   let j = step - 1;
+  
+    //   // Compare key with each element on the left of it until an element smaller than
+    //   // it is found.
+    //   // For descending order, change key<array[j] to key>array[j].
+    //   while (key < array[j] && j >= 0) {
+    //     array[j + 1] = array[j];
+    //     steps.push([...array]);
+    //     --j;
+    //   }
+    //   array[j + 1] = key;
+    //   steps.push([...array]); 
+    // } 
+    // (async () => {
+    //   for(let i = 0; i<steps.length; ++i){
+    //     console.log(steps[i]);
+    //     setBars(steps[i]);
+    //     await sleep(i);
+    //   }
+    // })();
   }
   return (
     <main>
