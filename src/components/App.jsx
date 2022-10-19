5d40e5cde26a7674adf2c5bfcd0b5901ebf494a3
Ankish-Nayak <ankish2000nayak@gmail.com>
2022-10-19 17:48:53 +0530

    ReImplemented insertionSort, Implemented bubbleSort

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,182 @@
-import React from "react";
+import React, { startTransition } from "react";
 import Sidebar from "./Sidebar";
 import Main from "./Main";
 
 export default function App() {
-  const [bars, setBars] = React.useState(allNewBars); 
+  const [bars, setBars] = React.useState(allNewBars);
+  const steps = React.useRef([]);
+  const timeOuts = React.useRef([]); // used to store setTimeout reference to clear it
+  React.useEffect(() => {
+    return () => {
+      timeOuts.current.forEach((timeOut) => clearTimeout(timeOut));
+    };
+  }, []);
   function randomBar(range) {
     return Math.floor(Math.random() * range);
   }
-  function allNewBars(n = 20, range = 500) {
+  function allNewBars(n = 5 , range = 500) {
     let res = [];
     for (let i = 0; i < n; ++i) {
-      res.push(randomBar(range));
-    } 
+      res.push({
+        value: randomBar(range),
+        selected: 0, // 0 comparing 1 compared and to be swap 2 sorted
+      });
+    }
     return res;
-  }  
-  function sortBars() { 
+  }
+  function sortBars() {
     setBars((prevBars) => {
       let res = [];
       for (let i = 0; i < prevBars.length; ++i) {
         res.push(prevBars[i]);
-      } 
-      res.sort((a, b) => a - b); 
+      }
+      res.sort((a, b) => a - b);
       return res;
     });
   }
+
+  function stepClear() {
+    while (steps.current.size > 0) {
+      steps.current.pop();
+    }
+  }
+  function stepCopy(bars) {
+    let newBars = [];
+    bars.forEach((bar) => {
+      newBars.push({
+        ...bar,
+      });
+    });
+    steps.current.push(newBars);
+  }
+  function insertionSort() {
+    console.log("called insertionSort");
+    // we will clear all the previous steps we had in steps array
+    stepClear();
+    const tempBars = [];
+
+    bars.forEach((bar) =>
+      tempBars.push({
+        ...bar,
+      })
+    );
+    console.log(tempBars);
+    console.log(tempBars.length);
+    for (let step = 1; step < tempBars.length; step++) {
+      console.log("ed");
+      let key = {
+        ...tempBars[step],
+      };
+      let j = step - 1;
+      // Compare key with each element on the left of it until an element smaller than
+      // it is found.
+      // For descending order, change key<tempBars[j] to key>tempBars[j].
+      if (key.value > tempBars[j].value) {
+        tempBars[j + 1].selected = tempBars[j].selected = 1;
+        stepCopy(tempBars);
+        // tempBars.forEach(bar => steps.current.push({
+        //   ...bar
+        // }));
+        tempBars[j + 1].selected = tempBars[j].selected = 0;
+        // do deep copy
+        stepCopy(tempBars);
+      }
+      console.log(step);
+      while (j >= 0 && key.value < tempBars[j].value) {
+        tempBars[j + 1].selected = tempBars[j].selected = 1;
+        // do deep copy
+        stepCopy(tempBars);
+        tempBars[j + 1] = { ...tempBars[j] };
+        tempBars[j + 1].selected = tempBars[j].selected = 0;
+        --j;
+      }
+      tempBars[j + 1] = { ...key };
+      console.log("d");
+      // do deep copy
+      stepCopy(tempBars);
+    }
+    // console.log(steps.current);
+    // console.log(tempBars);
+    start();
+  }
+  function start() {
+    console.log("called start");
+    console.log(steps.current);
+    const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+    let cnt = 0;
+    for (let i = 0; i < steps.current.length; ++i) {
+      console.log(steps.current[i]);
+      const b = steps.current[i];
+      let timeOut;
+      (() => {
+        timeOut = setTimeout((i) => {
+          setBars([...b]);
+        }, i * 500);
+      })();
+      timeOuts.current.push(timeOut);
+    }
+  }
+  function bubbleSort() {
+    const tempBars = [];
+    bars.forEach((bar) => {
+      tempBars.push({
+        ...bar,
+      });
+    });
+    stepClear();
+    for (let step = 0; step < tempBars.length; ++step) {
+      // loop to compare array elements
+      for (let i = 0; i < tempBars.length - step; ++i) {
+        // compare two adjacent elements
+        // change > to < to sort in descending order
+        if (
+          i + 1 < tempBars.length &&
+          tempBars[i].value < tempBars[i + 1].value
+        ) {
+          tempBars[i].selected = tempBars[i + 1].selected = 1;
+          stepCopy(tempBars);
+          tempBars[i].selected = tempBars[i + 1].selected = 0;
+          stepCopy(tempBars);
+        }
+        if (
+          i + 1 < tempBars.length &&
+          tempBars[i].value > tempBars[i + 1].value
+        ) {
+          tempBars[i].selected = tempBars[i + 1].selected = 1;
+          stepCopy(tempBars);
+          let temp = {
+            ...tempBars[i],
+          };
+          tempBars[i] = {
+            ...tempBars[i + 1],
+          };
+          tempBars[i + 1] = {
+            ...temp,
+          };
+          stepCopy(tempBars);
+          tempBars[i].selected = tempBars[i + 1].selected = 0;
+          stepCopy(tempBars);
+        }
+      }
+    }
+    start();
+  }
+  function selectionSort() {}
+  function mergeSort() {
+
+    
+  }
+  function quickSort() {}
+
   return (
     <main>
-      <Sidebar sortBars={sortBars} />
+      <Sidebar
+        insertionSort={insertionSort}
+        bubbleSort={bubbleSort}
+        selectionSort={selectionSort}
+        mergeSort={mergeSort}
+        quickSort={quickSort}
+      />
       <Main bars={bars} />
     </main>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,24 +1,37 @@
 import React from "react";
 
 export default function Sidebar(props) {
-//   console.log("side bar rendered");
-  const { sortBars } = props;
+  //   console.log("side bar rendered");
   return (
     <div className="sidebar">
       <h1 className="sidebar-heading">Algorithms</h1>
-      <div className="algorithms insertionSort selectedAlgorithm">
+      <div
+        className="algorithms insertionSort selectedAlgorithm"
+        onClick={() => props.insertionSort()}
+      >
         Insertion sort
       </div>
-      <div className="algorithms bubbleSort">Bubble Sort</div>
-      <div className="algorithms selectionSort">Selection Sort</div>
-      <div className="algorithms mergeSort">Merge Sort</div>
-      <div className="algorithms quickSort">Quick Sort</div>
+      <div className="algorithms bubbleSort" onClick={() => props.bubbleSort()}>
+        Bubble Sort
+      </div>
+      <div
+        className="algorithms selectionSort"
+        onClick={() => props.selectionSort()}
+      >
+        Selection Sort
+      </div>
+      <div className="algorithms mergeSort" onClick={() => props.mergeSort()}>
+        Merge Sort
+      </div>
+      <div className="algorithms quickSort" onClick={() => props.quickSort()}>
+        Quick Sort
+      </div>
       <div
         className="sidebar-button"
-        onClick={() => {
-          sortBars();
-        //   console.log("clicked");
-        }}
+        // onClick={() => {
+        //   sortBars();
+        //   //   console.log("clicked");
+        // }}
       >
         sort
       </div>
